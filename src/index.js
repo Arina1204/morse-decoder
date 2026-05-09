875cb60584248b6e95ae18801b28956c6400a5a2
Arina1204 <arinachirets@gmail.com>
2026-05-09 18:17:21 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-/*
 const MORSE_TABLE = {
   '.-': 'a',
   '-...': 'b',
@@ -37,8 +36,38 @@ const MORSE_TABLE = {
   '----.': '9',
   '-----': '0',
 };
-*/
 
-module.exports = function decode(/* expr */) {
-  throw new Error('Not implemented');
+module.exports = function decode(expr) {
+  const first = [];
+
+  for (let i = 0; i < expr.length; i += 10) {
+    first.push(expr.substring(i, i + 10));
+  }
+
+  let result = '';
+
+  for (let i = 0; i < first.length; i += 1) {
+    const block = first[i];
+
+    if (block === '**********') {
+      result += ' ';
+    } else {
+      let letter = '';
+      for (let j = 0; j < block.length; j += 2) {
+        const pair = block.substring(j, j + 2);
+
+        if (pair === '10') {
+          letter += '.';
+        }
+        if (pair === '11') {
+          letter += '-';
+        }
+        if (pair === '00') {
+          letter += '';
+        }
+      }
+      result += MORSE_TABLE[letter];
+    }
+  }
+  return result;
 };
